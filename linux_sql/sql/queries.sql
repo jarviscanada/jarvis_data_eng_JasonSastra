8db85721ef0ace959ae5ef0636664bfdcd8b0ecb
jason121301 <jason121301@gmail.com>
2025-12-03 21:42:38 -0500

    Added automation script for sql database

---
+++ linux_sql/sql/queries.sql
@@ -0,0 +1,25 @@
+\c host_agent
+
+CREATE TABLE IF NOT EXISTS public.host_info
+(
+    id               SERIAL PRIMARY KEY,
+    hostname         VARCHAR NOT NULL UNIQUE,
+    cpu_number       SMALLINT NOT NULL,
+    cpu_architecture VARCHAR NOT NULL,
+    cpu_model        VARCHAR NOT NULL,
+    cpu_mhz          DOUBLE PRECISION NOT NULL,
+    l2_cache         INT NOT NULL,
+    "timestamp"      TIMESTAMP,
+    total_mem        INT
+);
+
+CREATE TABLE IF NOT EXISTS public.host_usage
+(
+    "timestamp"      TIMESTAMP NOT NULL,
+    host_id          INT NOT NULL REFERENCES public.host_info(id),
+    memory_free      INT NOT NULL,
+    cpu_idle         SMALLINT NOT NULL,
+    cpu_kernel       SMALLINT NOT NULL,
+    disk_io          INT NOT NULL,
+    disk_available   INT NOT NULL
+);
